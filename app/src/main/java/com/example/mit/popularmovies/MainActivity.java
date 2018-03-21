a2a49a1b5347d4b2f58e1d11b289b3443db09c8d
em <emil.toms@gmail.com>
2018-03-21 20:21:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mit/popularmovies/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mit.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.imageView) ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+}
